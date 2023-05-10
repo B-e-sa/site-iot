eef6cfb5e91e52de59a003ae1e79ecd1baf35111
B-e-sa <joaospereira963@gmail.com>
2023-05-10 19:20:33 -0300

    remove tendencies array to json file

--- src/app/tendencies/page.tsx
+++ src/app/tendencies/page.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import tendencies from '../../utils/tendencies.json'
 
-interface ITopics {
+interface ITendencies {
     img: string;
     name: string;
     bgColor: string;
@@ -10,34 +11,9 @@ interface ITopics {
 }
 
 export default function Tendencies() {
-
-    const topics: ITopics[] = [
-        {
-            img: '/ia.svg',
-            name: 'Artificial Intelligence',
-            bgColor: 'bg-yellow-400',
-            summary: 'Sistemas em aprendizado, com raciocínio, percepção e compreensão da linguagem natural.',
-            link: '/ia'
-        },
-        {
-            img: '/blockchain.svg',
-            name: 'Blockchain',
-            bgColor: 'bg-green-400',
-            summary: 'Registro distribuído, armazenamento seguro e descentralizado de informações em uma rede.',
-            link: '/blockchain'
-        },
-        {
-            img: '/5g.svg',
-            name: '5g',
-            bgColor: 'bg-blue-400',
-            summary: 'A quinta geração de tecnologia de redes móveis, maior velocidade, menor latência, maior capacidade.',
-            link: '/fivegen'
-        }
-    ]
-
     return (
         <div className='flex-col my-5 mg:m-0 mg:flex-row flex mg:justify-evenly shrink items-center h-full w-full'>
-            {topics.map(({ img, name, bgColor, summary, link }) => {
+            {tendencies.map(({ img, name, bgColor, summary, link }: ITendencies) => {
                 return <Link shallow href={`tendencies/${link}`}>
                     <div className='flex flex-col items-center hover:scale-110 hover:m-10 mg:hover:m-0 transition-all'>
                         <div className={bgColor + ' w-80 h-80 flex justify-center items-center'}>
